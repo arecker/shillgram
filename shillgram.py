61bc088000e79c23acb580f4c671352ae12ce51b
Alex Recker <alex@reckerfamily.com>
2019-01-05 15:06:13 -0600

    contest: add comment iterator

--- shillgram.py
+++ shillgram.py
@@ -1,7 +1,47 @@
+import collections
+import random
 import subprocess
 
 import InstagramAPI
 import requests
+
+
+class CommentIterator(object):
+    def __init__(self, client=None, media_id=''):
+        self.client = client
+        self.media_id = media_id
+        self.has_more_comments = False
+
+    def __len__(self):
+        self.client.getMediaComments(self.media_id)
+        return self.client.LastJson['comment_count']
+
+    def __iter__(self):
+        max_id = ''
+
+        while True:
+            self.client.getMediaComments(self.media_id, max_id=max_id)
+            yield from reversed(self.client.LastJson['comments'])
+
+            if not self.client.LastJson.get('has_more_comments', False):
+                break
+
+            max_id = self.client.LastJson['next_max_id']
+
+
+class Post(object):
+
+    def __init__(self, client=None, url=''):
+        self.client = client
+        self.url = url
+
+        response = requests.get('http://api.instagram.com/oembed', params={
+            'url': self.url
+        }).json()
+
+        self.media_id = response['media_id']
+
+        self.comments = CommentIterator(client=client, media_id=self.media_id)
 
 
 def make_client(user='alex_recker'):
@@ -13,25 +53,27 @@ def make_client(user='alex_recker'):
     return client
 
 
-def get_media_id(post_url):
-    return requests.get('http://api.instagram.com/oembed', params={
-        'url': post_url
-    }).json()['media_id']
+def tags_from_text(text):
+    return [w[1:] for w in text.split(' ') if w.startswith('@')]
 
 
-def count_comments(post_url):
-    client = make_client()
-    media_id = get_media_id(post_url)
-    max_id = ''
-    count = 0
+def contest(url, max_entries=10):
 
-    while True:
-        client.getMediaComments(media_id, max_id=max_id)
-        count += len(client.LastJson['comments'])
+    """contest
 
-        if not client.LastJson.get('has_more_comments', False):
-            break
+    pick a random winner from comments, where each tagged friend is
+    one chance to win
+    """
 
-        max_id = client.LastJson['next_max_id']
+    participants = collections.defaultdict(list)
 
-    return count
+    for comment in Post(client=make_client(), url=url).comments:
+        user = comment['user']['username']
+        friends = tags_from_text(comment['text'])
+
+        while friends and len(participants[user]) < max_entries:
+            friend = friends.pop(0)
+            if friend != user and friend not in participants[user]:
+                participants[user].append(friend)
+
+    return participants
