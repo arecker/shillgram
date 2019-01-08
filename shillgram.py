157aedaa6eef935d0610de51343e62e236224e20
Alex Recker <alex@reckerfamily.com>
2019-01-08 07:55:12 -0600

    Handle 404

--- shillgram.py
+++ shillgram.py
@@ -1,13 +1,11 @@
 # -*- coding: utf-8 -*-
 
-"""shillgram
-
-Create a sweepstakes drawing, using instagram comments as entries.
-"""
+"""Create a sweepstakes drawing, using instagram comments as entries."""
 
 import argparse
 import json
 import logging
+import sys
 import urllib.parse
 import urllib.request
 
@@ -25,10 +23,15 @@ def make_logger():
 logger = make_logger()
 
 
+class FourOhFour(Exception):
+    pass
+
+
 def get_args():
     parser = argparse.ArgumentParser(description=__doc__)
     parser.add_argument('--url', '-u', help='instagram post permalink', required=True)
     parser.add_argument('--verbose', '-v', action='store_true', help='print debug logs', default=False)
+    parser.add_argument('--quiet', '-q', action='store_true', help='hide all output except winner', default=False)
     return parser.parse_args()
 
 
@@ -38,14 +41,18 @@ def make_request(url, params={}):
 
     request = urllib.request.Request(url)
 
-    with urllib.request.urlopen(request) as response:
-        return response.status, json.loads(response.read())
+    try:
+        with urllib.request.urlopen(request) as response:
+            return response.status, json.loads(response.read())
+    except urllib.error.HTTPError as e:
+        if e.code == 404:
+            raise FourOhFour
 
 
 def fetch_post(url):
     target = 'https://api.instagram.com/oembed'
     params = {'url': url}
-    code, response = make_request(target, params=params)
+    _, response = make_request(target, params=params)
     keys = ('media_id', 'title', 'author_name')
     return dict([(key, val) for key, val in response.items() if key in keys])
 
@@ -69,14 +76,21 @@ def yada_yada(text, max_chars=30, ending='...'):
 def main():
     args = get_args()
 
-    if args.verbose:
+    if args.quiet:
+        logger.handlers = []
+    elif args.verbose:
         logger.setLevel(logging.DEBUG)
 
     logger.debug('received args %s', vars(args))
 
     logger.info('looking up post from url %s', args.url)
-    post = fetch_post(args.url)
-    logger.info('found post "%s" by @%s', post['title'], post['author_name'])
+    try:
+        post = fetch_post(args.url)
+    except FourOhFour:
+        logger.error('cannot find post for url %s', args.url)
+        sys.exit(1)
+
+    logger.info('found post "%s" by @%s', yada_yada(post['title']), post['author_name'])
 
 
 if __name__ == '__main__':
