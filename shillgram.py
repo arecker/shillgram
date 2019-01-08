714eec3a3319c36df2a0b98683041ac556c67a08
Alex Recker <alex@reckerfamily.com>
2019-01-08 07:44:13 -0600

    Look up post

--- shillgram.py
+++ shillgram.py
@@ -6,8 +6,10 @@ Create a sweepstakes drawing, using instagram comments as entries.
 """
 
 import argparse
+import json
 import logging
-import urllib
+import urllib.parse
+import urllib.request
 
 
 def make_logger():
@@ -37,7 +39,31 @@ def make_request(url, params={}):
     request = urllib.request.Request(url)
 
     with urllib.request.urlopen(request) as response:
-        return response.status, response.read()
+        return response.status, json.loads(response.read())
+
+
+def fetch_post(url):
+    target = 'https://api.instagram.com/oembed'
+    params = {'url': url}
+    code, response = make_request(target, params=params)
+    keys = ('media_id', 'title', 'author_name')
+    return dict([(key, val) for key, val in response.items() if key in keys])
+
+
+def yada_yada(text, max_chars=30, ending='...'):
+    words = text.split(' ')
+    shortened_text = ''
+
+    while len(shortened_text) <= max_chars - len(ending):
+        try:
+            word = words.pop(0)
+            if shortened_text:
+                word = ' ' + word
+            shortened_text += word
+        except IndexError:
+            break
+
+    return '{}{}'.format(shortened_text, ending)
 
 
 def main():
@@ -47,6 +73,10 @@ def main():
         logger.setLevel(logging.DEBUG)
 
     logger.debug('received args %s', vars(args))
+
+    logger.info('looking up post from url %s', args.url)
+    post = fetch_post(args.url)
+    logger.info('found post "%s" by @%s', post['title'], post['author_name'])
 
 
 if __name__ == '__main__':
